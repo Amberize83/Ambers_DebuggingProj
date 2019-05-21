4c3c837e7d6127432815cea0b2f515429f152e1d
brbenne <bentking27@gmail.com>
2019-05-21 14:35:44 -0700

    Update Ambers_DebuggingProj.py

--- Ambers_DebuggingProj.py
+++ Ambers_DebuggingProj.py
@@ -10,3 +10,9 @@ def lone_sum(a, b, c): #Defined the function that will take 3 values at its use.
     else: #If none of the above have been triggered, the values of "a", "b", and "c" will be added.
         answer = a + b + c
     return answer
+
+print(lone_sum(1, 2, 3)) #Check if none are the same
+print(lone_sum(7, 7, 7)) #Check if all are the same
+print(lone_sum(1, 1, 3)) #Check if "a" and "b" are the same (return "c")
+print(lone_sum(1, 2, 1)) #Check if "a" and "c" are the same (return "b")
+print(lone_sum(1, 2, 2)) #Check if "b" and "c" are the same (return "a")
